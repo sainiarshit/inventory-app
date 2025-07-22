de1987ea52c9e9a5af238a3b2aee3b52be2c0182
ARSHIT SAINI <saini.arshit12@gmail.com>
2025-07-22 17:08:42 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,22 @@
-/**
- * The symbol to access the `TZDate`'s function to construct a new instance from
- * the provided value. It helps date-fns to inherit the time zone.
- */
-export declare const constructFromSymbol: unique symbol;
+export type Env = {
+    [key: string]: string | undefined;
+};
+export type LoadedEnvFiles = Array<{
+    path: string;
+    contents: string;
+    env: Env;
+}>;
+export declare let initialEnv: Env | undefined;
+export declare function updateInitialEnv(newEnv: Env): void;
+type Log = {
+    info: (...args: any[]) => void;
+    error: (...args: any[]) => void;
+};
+export declare function processEnv(loadedEnvFiles: LoadedEnvFiles, dir?: string, log?: Log, forceReload?: boolean, onReload?: (envFilePath: string) => void): Env[];
+export declare function resetEnv(): void;
+export declare function loadEnvConfig(dir: string, dev?: boolean, log?: Log, forceReload?: boolean, onReload?: (envFilePath: string) => void): {
+    combinedEnv: Env;
+    parsedEnv: Env | undefined;
+    loadedEnvFiles: LoadedEnvFiles;
+};
+export {};
